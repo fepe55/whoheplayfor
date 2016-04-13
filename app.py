bf436d1e3646e2427f33ae82786285724e8f4430
Mariano Ribotta <mribotta@cultura.gob.ar>
2016-04-13 16:13:23 -0300

    Added Bootstrap and using team logos instead of a select

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 def hello_world():
     PLAYERS_URL = "http://stats.nba.com/stats/commonallplayers?IsOnlyCurrentSeason=1&LeagueID=00&Season=2015-16"
     PLAYER_PICTURE_URL = "http://i.cdn.turner.com/nba/nba/.element/img/2.0/sect/statscube/players/large/%s.png"
+    TEAM_PICTURE_URL = "http://stats.nba.com/media/img/teams/logos/%s_logo.svg"
     r = requests.get(PLAYERS_URL)
     j = json.loads(r.text)
     nba_players = j['resultSets'][0]['rowSet']
@@ -19,14 +20,22 @@ def hello_world():
     for p in nba_players:
         # Roster status
         if p[3] != 0:
+            team = {
+                'city': p[8],
+                'name': p[9],
+                'abbreviation': p[10],
+                'code': p[11],
+                'picture': TEAM_PICTURE_URL % p[10],
+            }
+            if team not in teams:
+                teams.append(team)
+
             player = {
                 'name': p[2],
-                'team': p[10],
+                'team': team,
                 'picture': PLAYER_PICTURE_URL % p[6],
             }
             players.append(player)
-            if p[10] not in teams:
-                teams.append(p[10])
 
     teams = sorted(teams)
     player = random.choice(players)
