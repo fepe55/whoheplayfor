41c1888b2a870115492c933455b2383cd950c3d5
Fepe <fepe55@gmail.com>
2016-04-15 04:00:21 -0300

    Added levels, tidy-up of javascript and a ton more

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
 # -*- encoding: utf-8 -*-
 
-import json
 import requests
-from flask import (Flask, render_template, )
+from flask import (Flask, render_template, abort, )
 app = Flask(__name__)
 
 
 @app.route('/results/<code>')
-def show_user_profile(code):
-    # code: rounds(3) + n times (player_id(8), guess_id(2))
+def results(code):
+    # code: level(1) + rounds(3) + n times (player_id(8), guess_id(2))
 
+    level = int(code[:1])
+    code = code[1:]
     rounds = int(code[:3])
-    guesses_str = code[3:]
+    code = code[3:]
     guesses = []
     for i in xrange(rounds):
-        guess_str = guesses_str[10*i:10*i+10]
+        guess_str = code[10*i:10*i+10]
         guess = {
             'round': i+1,
             'player_id': int(guess_str[:8]),
@@ -27,7 +28,11 @@ def show_user_profile(code):
     TEAM_PICTURE_URL = "http://stats.nba.com/media/img/teams/logos/%s_logo.svg"
 
     r = requests.get(PLAYERS_URL)
-    j = json.loads(r.text)
+    try:
+        j = r.json()
+    except ValueError:
+        abort(500, "There's been a problem fetching info from NBA.com")
+
     nba_players = j['resultSets'][0]['rowSet']
     proper_guesses = {}
     for i in xrange(rounds):
@@ -67,17 +72,27 @@ def show_user_profile(code):
         final_guesses = []
         for i in xrange(rounds):
             final_guesses.append(proper_guesses[i+1])
-    return render_template('results.html', guesses=final_guesses)
+    return render_template('results.html', guesses=final_guesses,
+                           level=level)
 
 
-@app.route('/')
-def main():
+@app.route('/lvl/<level>')
+def game(level):
+    level = int(level)
+    if level < 0 or level > 4:
+        abort(404)
+
     PLAYERS_URL = "http://stats.nba.com/stats/commonallplayers?IsOnlyCurrentSeason=1&LeagueID=00&Season=2015-16"
     PLAYER_PICTURE_URL = "http://i.cdn.turner.com/nba/nba/.element/img/2.0/sect/statscube/players/large/%s.png"
     TEAM_PICTURE_URL = "http://stats.nba.com/media/img/teams/logos/%s_logo.svg"
 
     r = requests.get(PLAYERS_URL)
-    j = json.loads(r.text)
+    try:
+        j = r.json()
+    except ValueError:
+        print dir(r)
+        abort(500, "There's been a problem fetching info from NBA.com")
+
     nba_players = j['resultSets'][0]['rowSet']
     players = []
     teams = []
@@ -105,7 +120,14 @@ def main():
 
     teams = sorted(teams)
     # player = random.choice(players)
-    return render_template('whpf.html', players=players, teams=teams)
+    return render_template('whpf.html', players=players, teams=teams,
+                           level=level)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=8000)
