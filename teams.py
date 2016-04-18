64398b5c613c6b16a8fed2614e2cc422d5dd4a68
Mariano Ribotta <mribotta@cultura.gob.ar>
2016-04-18 15:22:36 -0300

    Teams

---
+++ teams.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+ATLANTIC_TEAMS = ['celtics', 'nets', 'knicks', 'sixers', 'raptors', ]
+CENTRAL_TEAMS = ['bulls', 'cavaliers', 'pistons', 'pacers', 'bucks', ]
+SOUTHEAST_TEAMS = ['hawks', 'hornets', 'heat', 'magic', 'wizards', ]
+
+EAST_TEAMS = ATLANTIC_TEAMS + CENTRAL_TEAMS + SOUTHEAST_TEAMS
+
+NORTHWEST_TEAMS = ['nuggets', 'timberwolves', 'thunder', 'blazers', 'jazz', ]
+PACIFIC_TEAMS = ['warriors', 'clippers', 'lakers', 'suns', 'kings', ]
+SOUTHWEST_TEAMS = ['mavericks', 'rockets', 'grizzlies', 'pelicans', 'spurs', ]
+
+WEST_TEAMS = NORTHWEST_TEAMS + PACIFIC_TEAMS + SOUTHWEST_TEAMS
+
+ALL_TEAMS = EAST_TEAMS + WEST_TEAMS
+
+PLAYOFF_TEAMS = [
+    'warriors', 'rockets',
+    'clippers', 'blazers',
+    'thunder', 'mavericks',
+    'spurs', 'grizzlies',
+    'cavaliers', 'pistons',
+    'hawks', 'celtics',
+    'heat', 'hornets',
+    'raptors', 'pacers',
+]
