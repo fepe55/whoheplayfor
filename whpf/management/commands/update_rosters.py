b622df2eb4cec029237da0a6ed55dcb1d6e07a25
Fepe <fepe55@gmail.com>
2016-06-08 15:03:13 -0300

    Update players works, albeit a little too verbose

--- whpf/management/commands/update_rosters.py
+++ whpf/management/commands/update_rosters.py
@@ -1,7 +1,10 @@
 # coding=utf-8
+import time
+import requests
 
 from django.core.management.base import BaseCommand
 
+from whpf.models import (Player, Team, )
 from whpf.helpers import get_players_api
 
 """
@@ -19,18 +22,61 @@ from whpf.helpers import get_players_api
 11 - "TEAM_CODE",
 12 - "GAMES_PLAYED_FLAG"
 """
+
+
 class Command(BaseCommand):
     help = 'Updates players'
 
     def handle(self, *args, **kwargs):
+        Player.all_players.update(active=False)
         nba_players = get_players_api()
+        if not nba_players:
+            print "Error with NBA.com"
+            return
         for p in nba_players:
-            player_qs = Player.objects.filter(nba_id)
-            # If there is a player, we update it
+            # I'm guessing teams never change. if they do, all hell breaks
+            # loose... or I just change a thing or two
+            if not p[7]:
+                continue
+            team = Team.objects.get(nba_id=p[7])
+            player_qs = Player.all_players.filter(nba_id=p[0])
+            # If there is a player with that ID, we update it
             if player_qs.exists():
-                # There SHOULD only be one
-                player = player_qs.get()
+                # There SHOULD only be one.
+                # player = player_qs.get()
+                print "update", p[2]
+                player_qs.update(
+                    name=p[2],
+                    team=team,
+                    code=p[6],
+                    active=True,
+                )
+
             # If there isn't a player with that ID, we create it
             else:
-                Crearlo
+                Player.all_players.create(
+                    nba_id=p[0],
+                    name=p[2],
+                    team=team,
+                    code=p[6],
+                    active=True,
+                )
+                print "create", p[2]
 
+        # Lastly, we find the faceless-ones
+
+        for p in Player.all_players.all():
+            r = requests.get(p.picture)
+            if r.status_code == 200:
+                p.faceless = False
+                p.save()
+                print p, 'has a face!'
+            elif r.status_code == 404:
+                print p, 'has no face ------------------------------'
+                p.faceless = True
+                p.save()
+            else:
+                print 'error with', p, '++++++++++++++++++++++++++++'
+
+            # Sleep to avoid a possible anti-throttling from the server
+            time.sleep(2)
