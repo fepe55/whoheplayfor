1c95d4ab061a53f4cccfdf790ba1fe5df00856e0
Mariano Ribotta <fepe55@gmail.com>
2019-11-21 17:36:33 -0300

    Remove every Django auth view, except logout

--- whoheplayfor/urls.py
+++ whoheplayfor/urls.py
@@ -1,10 +1,11 @@
 """WhoHePlayFor URL Configuration"""
 from django.contrib import admin
 from django.urls import (path, include, )
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', include('social_django.urls', namespace='social')),
-    path('', include('django.contrib.auth.urls')),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('djadmin/', admin.site.urls),
     path('', include('whpf.urls', namespace="whpf")),
 ]
