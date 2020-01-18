1276f9485d96e29f62de1daf33b35efa5d289d1b
Fepe <fepe55@gmail.com>
2020-01-18 11:24:38 -0300

    Opened a file with the with statement

--- whpf/management/commands/find_faceless.py
+++ whpf/management/commands/find_faceless.py
@@ -10,24 +10,22 @@ def get_faceless():
         "uploads/headshots/nba/latest/260x190/%s.png"
 
     players = get_players()
-    f = open('faceless.txt', 'w')
-    for p in players[300:]:
-        if p[3] != 0:
-            print(p[0], p[2])
-            picture = PLAYER_PICTURE_URL % p[6]
-            r = requests.get(picture)
-            if r.status_code == 200:
-                print('has a face!')
-            elif r.status_code == 404:
-                f.write(str(p[0]))
-                f.write('\n')
-                print('has no face')
-            else:
-                print('error', p)
+    with open('faceless.txt', 'w') as f:
+        for p in players[300:]:
+            if p[3] != 0:
+                print(p[0], p[2])
+                picture = PLAYER_PICTURE_URL % p[6]
+                r = requests.get(picture)
+                if r.status_code == 200:
+                    print('has a face!')
+                elif r.status_code == 404:
+                    f.write(str(p[0]))
+                    f.write('\n')
+                    print('has no face')
+                else:
+                    print('error', p)
 
-        time.sleep(2)
-
-    f.close()
+            time.sleep(2)
 
 
 # meant to be run from the project root as `python -m utils.find_faceless`
