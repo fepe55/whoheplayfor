36d82702d2714f7e2888baf84e265875d037c876
Fepe <fepe55@gmail.com>
2020-01-18 11:23:20 -0300

    Print is a function

--- whpf/management/commands/find_faceless.py
+++ whpf/management/commands/find_faceless.py
@@ -13,17 +13,17 @@ def get_faceless():
     f = open('faceless.txt', 'w')
     for p in players[300:]:
         if p[3] != 0:
-            print p[0], p[2],
+            print(p[0], p[2])
             picture = PLAYER_PICTURE_URL % p[6]
             r = requests.get(picture)
             if r.status_code == 200:
-                print 'has a face!'
+                print('has a face!')
             elif r.status_code == 404:
                 f.write(str(p[0]))
                 f.write('\n')
-                print 'has no face'
+                print('has no face')
             else:
-                print 'error', p
+                print('error', p)
 
         time.sleep(2)
 
