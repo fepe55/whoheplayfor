b720c3ff0cd049353a86479bff960e40dd2e220a
Fepe <fepe55@gmail.com>
2021-09-08 10:30:39 -0300

    Going full env variables

--- whoheplayfor/settings.py
+++ whoheplayfor/settings.py
@@ -16,6 +16,8 @@ from django.urls import reverse_lazy
 # HEROKU
 # import dj_database_url
 
+TRUE_VALUES = ['true', '1', 't', 'y', 'yes']
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -129,8 +131,13 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = os.environ.get('STATIC_URL', '/static/')
+STATIC_ROOT = os.environ.get(
+    'STATIC_ROOT', os.path.join(PROJECT_ROOT, 'staticfiles')
+)
+
+MEDIA_URL = os.environ.get('MEDIA_URL', '')
+MEDIA_ROOT = os.environ.get('MEDIA_ROOT', '')
 
 
 # LOGIN_URL = reverse_lazy("login")
@@ -138,8 +145,49 @@ LOGIN_URL = reverse_lazy("whpf:home")
 LOGIN_REDIRECT_URL = reverse_lazy("whpf:home")
 LOGOUT_REDIRECT_URL = reverse_lazy("whpf:home")
 
+admin_names = os.environ.get('ADMIN_NAMES').split(',')
+admin_emails = os.environ.get('ADMIN_EMAILS').split(',')
+ADMINS = list(zip(admin_names, admin_emails))
 
-try:
-    from .local_settings import *  # noqa: F401,F403
-except ModuleNotFoundError:
-    pass
+EMAIL_ENABLED = os.environ.get("EMAIL_ENABLED", 'False').lower() in TRUE_VALUES
+
+if EMAIL_ENABLED:
+    EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'False').lower() in TRUE_VALUES
+    EMAIL_HOST = os.environ.get('EMAIL_HOST')
+    EMAIL_PORT = int(os.environ.get('EMAIL_PORT'))
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+    SERVER_EMAIL = os.environ.get('SERVER_EMAIL')  # Address for errors
+    DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')  # For sendmail
+
+
+SOCIAL_AUTH_ENABLED = os.environ.get("SOCIAL_AUTH_ENABLED", 'False').lower() in TRUE_VALUES  # noqa: E501
+
+if SOCIAL_AUTH_ENABLED:
+    SOCIAL_AUTH_FACEBOOK_KEY = os.environ.get('SOCIAL_AUTH_FACEBOOK_KEY')
+    SOCIAL_AUTH_FACEBOOK_SECRET = os.environ.get('SOCIAL_AUTH_FACEBOOK_SECRET')
+
+    SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.environ.get('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY')  # noqa: E501
+    SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.environ.get('SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET')  # noqa: E501
+
+    SOCIAL_AUTH_TWITTER_KEY = os.environ.get('SOCIAL_AUTH_TWITTER_KEY')
+    SOCIAL_AUTH_TWITTER_SECRET = os.environ.get('SOCIAL_AUTH_TWITTER_SECRET')
+
+    SOCIAL_AUTH_REDDIT_KEY = os.environ.get('SOCIAL_AUTH_REDDIT_KEY')
+    SOCIAL_AUTH_REDDIT_SECRET = os.environ.get('SOCIAL_AUTH_REDDIT_SECRET')
+    SOCIAL_AUTH_REDDIT_AUTH_EXTRA_ARGUMENTS = {'duration': 'permanent'}
+
+    SOCIAL_AUTH_REDIRECT_IS_HTTPS = True
+
+
+# SENTRY
+SENTRY_SDK_ENABLED = os.environ.get("SENTRY_SDK_ENABLED", 'False').lower() in TRUE_VALUES  # noqa: E501
+
+if SENTRY_SDK_ENABLED:
+    import sentry_sdk
+    from sentry_sdk.integrations.django import DjangoIntegration
+
+    sentry_sdk.init(
+        dsn=os.environ.get('SENTRY_SDK_DSN'),
+        integrations=[DjangoIntegration()]
+    )
