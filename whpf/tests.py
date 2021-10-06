58118d2f8fb8306528ecb852a2bc8ac2b1ecb8e9
Fepe <fepe55@gmail.com>
2021-10-06 13:06:47 -0300

    Deepsource PY-D0003

--- whpf/tests.py
+++ whpf/tests.py
@@ -10,30 +10,37 @@ class BaseTestCase(TestCase):
 
 class BasicAccessTestCase(BaseTestCase):
     def _test_url(self, url, expected_status_code=200):
+        """Helper function for testing url"""
         response = self.client.get(url)
         self.assertEqual(response.status_code, expected_status_code)
 
     def test_home(self):
+        """Test home view"""
         url = reverse('whpf:home')
         self._test_url(url)
 
     def test_tv(self):
+        """Test TV view"""
         url = reverse('whpf:tv')
         self._test_url(url)
 
     def test_faq(self):
+        """Test FAQ view"""
         url = reverse('whpf:faq')
         self._test_url(url)
 
     def test_scoreboard(self):
+        """Test scoreboard view"""
         url = reverse('whpf:scoreboard')
         self._test_url(url)
 
     def test_stats(self):
+        """Test stats view"""
         url = reverse('whpf:stats')
         self._test_url(url)
 
     def test_teams_stats(self):
+        """Test teams stats view"""
         teams = Team.objects.all()
         for team in teams:
             url = team.stats_url()
